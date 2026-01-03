0e85ff8154bfbacd59f8e8f2b553de8fe482e6fa
Siva Krishna <rskrishna.cloudtech@gmail.com>
2026-01-03 13:06:43 +0530

    Created the terraform practice scripts

---
+++ ec2/ec2.tf
@@ -0,0 +1,40 @@
+# Create Security group with rules.
+# Inbound - Allow everything or only the port 22 for SSh connection.
+# Outbounf - Allow everything.
+
+resource "aws_security_group" "Allow_SSH" {
+  name        = "Allow_SSH"
+  description = "Allowing SSH access"
+
+  ingress = {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cide_blocks = ["0.0.0.0/0"]
+  }
+
+  egress = {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name      = "allow_ssh"
+    CreatedBy = "Siva Krishna"
+  }
+
+}
+
+# Creating an EC2 instance.
+resource "aws_instance" "db_instance" {
+  ami                    = "ami-090252cbe067a9e58"
+  vpc_security_group_ids = [aws_security_group.Allow_SSH.id]
+  instance_type          = "t2.micro"
+
+  tags = {
+    Name = "db"
+  }
+}
+
