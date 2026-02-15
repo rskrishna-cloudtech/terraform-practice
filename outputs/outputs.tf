dcd5a1f26361a5d87c13b4f11c9cc78f626a6ff5
Siva Krishna <rskrishna.cloudtech@gmail.com>
2026-02-15 10:52:01 +0530

    Corrected few syntaxes and updated in outputs in terraform practice

--- outputs/outputs.tf
+++ outputs/outputs.tf
@@ -1,5 +1,5 @@
 # This is used to store the public IP of the instance created.
 output "instance_public_ip" {
-  value       = aws_instance.db_instance.public_ip
+  value       = aws_instance.db_instance[*].public_ip
   description = "The public IP address of the EC2 instance"
 }
