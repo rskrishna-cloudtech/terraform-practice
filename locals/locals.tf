b49b432e5eda80482a56812f069dc81f49d95015
Siva Krishna <rskrishna.cloudtech@gmail.com>
2026-02-15 14:50:42 +0530

    Corrected the syntaxes and updated the data source filters in locals in terraform practice

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,7 +1,7 @@
 # Creating few local values to avoid repetition of code.
 locals {
-  ami_id = "ami-090252cbe067a9e58"
-  instance_type = var.instance_names[count.index] == "frontend" ? "t2.micro" : "t3.medium"
+  ami_id        = "ami-0220d79f3f480ecf5"
+  instance_type = var.instance_names == "frontend" ? "t3.micro" : "t3.small"
 
   tags = {
     Name = "locals"
