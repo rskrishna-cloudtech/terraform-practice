90305888687d2afbfa131491e68142e74911f895
Siva Krishna <rskrishna.cloudtech@gmail.com>
2026-02-15 19:57:50 +0530

    updated the provisioners in terraform practice

--- provisioners/remote-exec/ec2.tf
+++ provisioners/remote-exec/ec2.tf
@@ -6,14 +6,14 @@ resource "aws_security_group" "Allow_SSH" {
   name        = var.sg_name
   description = var.sg_description
 
-  ingress = {
+  ingress {
     from_port   = var.ssh_port
     to_port     = var.ssh_port
     protocol    = var.protocol
-    cide_blocks = var.cidr_blocks
+    cidr_blocks = var.cidr_blocks
   }
 
-  egress = {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -45,14 +45,15 @@ resource "aws_instance" "db_instance" {
     type     = "ssh"
     user     = "ec2-user"
     password = "DevOps321"
-    host     = self.private_ip
+    host     = self.public_ip
   }
 
   # remote provisioner to install the ansible and then run the ansible-playbook to install the nginx once the instance is created.
   provisioner "remote-exec" {
     inline = [
       "sudo dnf install ansible -y",
-      "ansible-playbook -i private-ips.txt nginx.yml",
+      # "ansible-playbook -i private-ips.txt nginx.yml",
+      "sudo dnf install nginx -y",
       "sudo systemctl start nginx"
     ]
   }
