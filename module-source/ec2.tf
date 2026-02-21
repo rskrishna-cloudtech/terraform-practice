1dcfc7d2a1df6dae6e261a46f525663573f28292
Siva Krishna <rskrishna.cloudtech@gmail.com>
2026-02-21 14:59:02 +0530

    Corrected the syntax erros in module-source  in terraform practice

--- module-source/ec2.tf
+++ module-source/ec2.tf
@@ -8,14 +8,14 @@ resource "aws_security_group" "Allow_SSH" {
   name        = "Allow_SSH"
   description = "Allowing SSH access"
 
-  ingress = {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cide_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress = {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
--- module-source/variables.tf
+++ module-source/variables.tf
@@ -1,17 +1,17 @@
 # Variable to store the ami id.
 variable "ami_id" {
-  type = string
-  default = "ami-090252cbe067a9e58"
+  type    = string
+  default = "ami-0220d79f3f480ecf5"
 }
 
 # Variable to store the instance types.
 variable "instance_type" {
-  type = string
+  type    = string
   default = "t3.micro"
 }
 
 # Variable to store the tag details.
 variable "instance_tags" {
-  type = map
+  type    = map(any)
   default = {}
 }
